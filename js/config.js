e397b3e58018cdaa31e85d2d5316b32721e0fbd8
nharmonydeveloper <developer@nharmony.com>
2013-12-16 15:53:29 -0500

    fa

--- js/config.js
+++
@@ -1,6 +0,0 @@
-var config = {
-	appTitle: 'Invoice Zoom',
-	operatorUrl: 'http://operator.invoicezoom.dev/app_validateDomain.php',
-	loginUrl: '/app_login.php',
-	receiverUrl: '/app_receiver.php'
-}; 
